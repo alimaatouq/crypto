a2f0e22cbbbc40c4179bc30e43ec457887c9d99f
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2024-03-28 13:48:15 +0200

    Update crypto_app_ml.py

--- crypto_app_ml.py
+++ crypto_app_ml.py
@@ -336,8 +336,12 @@ if menu_id == "Home":
         for art in articles:
             #get image
             image=str(art.find('div', {'class': 'img-sized'}))
-            image= re.findall(r'\bsrc=".+\b"', image)[0].replace('src="', "").replace('"',"")
-            images.append(image)
+            if image_tag:
+                image_src = image_tag.find('img').get('src')
+                images.append(image_src)
+            else:
+                images.append(None)
+            
             #get title
             title = art.find('a',{'class': 'article__title'}).get_text()
             titles.append(title)
